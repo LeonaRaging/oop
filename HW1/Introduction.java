84ccb51257567c731578b4b0ab8b236364dfd88f
HoanhTQ <24020142@vnu.edu.vn>
2025-09-19 14:28:50 +0700

    HW1 B Section

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+      System.out.println("Ho va ten: Truong Quang Hoanh \t Ma sinh vien: 24020142 \t Lop: INT2204 4 \t Username: LeonaRaging \t Email: 24020142@vnu.edu.vn");
+      for (int i = 9; i >= 1; i--) {
+        System.out.println(i + " bottles of beer on the wall, " + i + " bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+      }
+      System.out.println("No more bottles of beer on the wall");
+    }
+}
